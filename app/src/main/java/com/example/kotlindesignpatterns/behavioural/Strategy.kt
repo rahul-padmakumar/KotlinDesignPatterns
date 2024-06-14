7807ac73651d1a4f9058e33aa6a868c8a7763d94
rahul <rahulp5875@gmail.com>
2024-06-14 08:56:54 +0530

    Add comment in strategy pattern.

--- app/src/main/java/com/example/kotlindesignpatterns/behavioural/Strategy.kt
+++ app/src/main/java/com/example/kotlindesignpatterns/behavioural/Strategy.kt
@@ -1,5 +1,9 @@
 package com.example.kotlindesignpatterns.behavioural
 
+/**
+ * Use the Strategy pattern when you want to use different variants of an algorithm within an object and be able to switch from one algorithm to another during runtime.
+ */
+
 class Printer(private val formattingStrategy: (String) -> String){
 
     fun print(text: String) = println(formattingStrategy(text))
